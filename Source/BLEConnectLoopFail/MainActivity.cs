343bb289573ea60f4fc470a28efb7a1478a9d059
German Enriquez <german.enriquez@glucostratus.com>
2018-04-23 21:33:52 +0300

    Sample project with reproduction of issue #297

---
+++ Source/BLEConnectLoopFail/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Plugin.BLE;
+using Plugin.BLE.Abstractions;
+
+namespace BLEConnectLoopFail
+{
+    [Activity(Label = "BLEConnectLoopFail", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            DoBluetoothConnectLoop();
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+
+        public async void DoBluetoothConnectLoop()
+        {
+            var manager = CrossBluetoothLE.Current;
+            var adapter = manager.Adapter;
+
+            var sw = Stopwatch.StartNew();
+            var pairedDeviceId = Guid.Parse("00000000-0000-0000-0000-a0e6f888c512");
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine($"$> Attempt connect (last attempt {sw.ElapsedMilliseconds}ms ago)");
+                    var cts = new CancellationTokenSource(1000);
+                    await adapter.ConnectToKnownDeviceAsync(pairedDeviceId, new ConnectParameters(true), cts.Token);
+                }
+                catch (TaskCanceledException)
+                {
+                }
+            }
+        }
+    }
+}
+
